2ee51ef6c58651815cf96b8eccc649aab880350f
Rituraj <riturajreso@gmail.com>
2018-04-24 00:42:24 +0530

    Added in include folder

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "tiger";
-	$database = "ebooks";
-
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $database);
-?>
